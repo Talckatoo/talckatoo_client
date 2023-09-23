f8aceb501f56b03603a477e4d3e5c7712a10e70b
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-09-22 23:07:11 -0400

    add prettier and husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
-import './index.css'
-import {UserContextProvider} from "./context/user-context"
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { UserContextProvider } from "./context/user-context";
 import { injectStyle } from "react-toastify/dist/inject-style";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // CALL IT ONCE IN YOUR APP
 injectStyle();
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-    <>
+  <>
     <UserContextProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </UserContextProvider>
-        <ToastContainer />
-    </>
-        );
+    <ToastContainer />
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
