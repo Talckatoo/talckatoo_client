b0781072d391b1cc96821acbff6c663b2154ccf1
Mohamed Aachour <aachourmohameddeveloper@gmail.com>
2023-10-19 06:36:28 +0100

    Change the name of jhon

--- src/constants/testimonials.ts
+++ src/constants/testimonials.ts
@@ -1,20 +1,20 @@
 export const testimonials = [
   {
     image: "https://loremflickr.com/320/240",
-    name: "John Doe",
-    title: "CEO",
+    name: "Anastasia Dan",
+    title: "UX Board",
     text: "Talkcatoo has revolutionized how our team communicates. The real-time translation feature means we can work seamlessly with international partners without missing a beat!",
   },
   {
     image: "https://loremflickr.com/320/240",
-    name: "John Doe",
-    title: "CEO",
+    name: "Roman Level",
+    title: "AppName",
     text: "Being a freelancer means constantly communicating with clients from all over the world. Talkcatoo makes this so much easier. No more language barriers, just clear communication. It's a game-changer!",
   },
   {
     image: "https://loremflickr.com/320/240",
-    name: "John Doe",
-    title: "CEO",
+    name: "Akex Sackett",
+    title: "Reform Layouts",
     text: "Our projects used to get delayed due to miscommunication. With Talkcatoo, everyone's on the same page, literally. The intuitive design means even our less tech-savvy team members can use it with ease.",
   },
 ];
