e4f84b877321d13bf6e7bc1ed5c7a4cd1c32246c
tarikbouari <tarikbouari@gmail.com>
2023-12-13 02:27:04 +0000

    Update hero section base on figma design

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -1,30 +1,40 @@
 import Button from "../../UI/Button";
 import { useNavigate } from "react-router-dom";
+import hero from "../../../public/assets/img/herotrans.png"
 
 const Hero = () => {
   const navigate = useNavigate();
   return (
-    <section className="container mt-[5rem] p-1">
-      <div className="flex flex-col items-center justify-center ">
-        <h1 className="head-text text-center max-w-[983px] max-lg:max-w-[700px] z-[1]">
-          Talckatoo: Bridging Conversations Across Languages
-        </h1>
-        <p className="sub-title-text text-center mt-4 max-w-[768px] max-lg:max-w-[700px] z-[1]">
-          Seamlessly chat with individuals across language barriers. No more
-          copy-pasting. Just real-time, in-chat translations.
-        </p>
-        <Button
-          type="button"
-          className="bg-primary-500 text-white mt-8 z-[1]"
-          onClick={() => {
-            navigate("/sign-up");
-          }}
-        >
-          Try Talckatoo Now
-        </Button>
+    <section className="container p-1">
+      <div className="flex  items-center justify-center px-[5rem]">
+        <div className="i">
+          <div>
+            <h1 className="head-text text-center max-w-[983px] max-lg:max-w-[700px] z-[1]">
+              Talckatoo: Bridging Conversations Across Languages
+            </h1>
+            <p className="sub-title-text text-center mt-4 max-w-[768px] max-lg:max-w-[700px] z-[1]">
+               Seamlessly chat with individuals across language barriers. No more
+              copy-pasting. Just real-time, in-chat translations.
+            </p>
+            <Button
+              type="button"
+              className="bg-primary-500 text-white mt-8 z-[1]"
+              onClick={() => {
+             navigate("/sign-up");
+             }}
+            >
+              Try Talckatoo Now
+            </Button>
+          </div>
+          <div>
+            
+          </div>   
+        </div>
+        
+        
         <div className="mt-[4rem] z-[1]">
           <img
-            src="/assets/img/hero.svg"
+            src={hero}
             alt="hero"
             className="w-[1000px] max-lg:w-[700px] h-full object-contain"
           />
