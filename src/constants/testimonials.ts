68977613c8355f7d7c22a64af31e7874a9eabbda
tarikbouari <tarikbouari@gmail.com>
2023-12-15 20:50:56 +0000

    Update testimonials content

--- src/constants/testimonials.ts
+++ src/constants/testimonials.ts
@@ -1,20 +1,20 @@
 export const testimonials = [
   {
-    image: "https://loremflickr.com/320/240",
+    image: "/assets/img/ana.png",
     name: "Anastasia Dan",
-    title: "UX Board",
-    text: "Talkcatoo has revolutionized how our team communicates. The real-time translation feature means we can work seamlessly with international partners without missing a beat!",
+    title: "Product Developer at Webflow",
+    text: "Talkcatoo has revolutionized how our team communicates. The real-time translation feature means we can work seamlessly with international partners without missing  a beat!",
   },
   {
-    image: "https://loremflickr.com/320/240",
-    name: "Roman Level",
-    title: "AppName",
+    image: "/assets/img/adam.png",
+    name: "Adam Smith",
+    title: "Web Designer at Airtable",
     text: "Being a freelancer means constantly communicating with clients from all over the world. Talkcatoo makes this so much easier. No more language barriers, just clear communication. It's a game-changer!",
   },
   {
-    image: "https://loremflickr.com/320/240",
-    name: "Akex Sackett",
-    title: "Reform Layouts",
+    image: "/assets/img/mike.png",
+    name:  "Mike Warren",
+    title: "Product Manager at Zapier",
     text: "Our projects used to get delayed due to miscommunication. With Talkcatoo, everyone's on the same page, literally. The intuitive design means even our less tech-savvy team members can use it with ease.",
   },
 ];
