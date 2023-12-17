eae27bdcd384af28fe44e36a58ece5ed5bbace10
tarikbouari <tarikbouari@gmail.com>
2023-12-17 23:38:50 +0000

    Update file

--- src/util/url.ts
+++ src/util/url.ts
@@ -1,7 +1,9 @@
 export let BASE_URL: string;
 
-if (import.meta.env.VITE_HOST == "production") {
-  BASE_URL = "https://talckatoo-250985c83f7c.herokuapp.com";
-} else {
-  BASE_URL = "http://localhost:8000";
-}
+// if (import.meta.env.VITE_HOST == "production") {
+//   BASE_URL = "https://talckatoo-250985c83f7c.herokuapp.com";
+// } else {
+//   BASE_URL = "http://localhost:8000";
+// }
+
+BASE_URL = "https://talckatoo-250985c83f7c.herokuapp.com";
