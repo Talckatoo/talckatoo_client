3ca4bbd85152f17b0797a614da7153776b397ced
tarikbouari <tarikbouari@gmail.com>
2023-12-19 22:26:38 +0000

    Resize features card

--- src/components/LandingPage/Features.tsx
+++ src/components/LandingPage/Features.tsx
@@ -17,13 +17,13 @@ const Features = () => {
         </div>
         <div className="mt-[5rem] grid sm:grid-cols-2 md:grid-rows-2 gap-4 sm:justify-center ">
            
-           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex items-center  rounded-[25px]">
+           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex items-center  max-sm:gap-4 rounded-[25px]">
             <img src="/assets/icons/traduction.png" alt="translate" className=" w-[42px] h-[42px]" />
-            <span className="text-center text-[20px]"> Text-to-Text Translation</span>       
+            <span className="text-center max-sm:text-[18px]  text-[20px]"> Text-to-Text Translation</span>       
           </div>
-           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex  items-center  rounded-[25px]">
+           <div className="bg-light-bg border max-sm:gap-4 border-[#EFF0F6] p-4 flex  items-center  rounded-[25px]">
             <img src="/assets/icons/voice.png" alt="translate" className=" w-[42px] h-[42px]" />
-            <span className="text-center text-[20px]">Voice-to-Voice Translation</span>
+            <span className="text-center max-sm:text-[18px]  text-[20px]">Voice-to-Voice Translation</span>
            </div>
            <div className="bg-light-bg border border-[#EFF0F6] p-4  items-center gap-8 rounded-[25px] hidden md:flex">
             <img src="/assets/icons/friend.png" alt="translate" className=" w-[42px] h-[42px]" />
@@ -40,11 +40,11 @@ const Features = () => {
           <img src="/assets/img/iPhone.png" alt="iPhone" className="md:max-w-[50%] h-full object-contain" />
         </div>
         <div className="grid sm:grid-cols-2 gap-4 sm:justify-center ">
-          <div className="bg-light-bg border border-[#EFF0F6] p-4 flex items-center gap-8 rounded-[25px]  md:hidden ">
+          <div className="bg-light-bg border border-[#EFF0F6] p-4 flex justify-center  items-center gap-8 rounded-[25px]  md:hidden ">
             <img src="/assets/icons/friend.png" alt="translate" className=" w-[42px] h-[42px]" />
             <span>Friend Matching </span>
            </div>
-           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex items-center gap-8 rounded-[25px] md:hidden ">
+           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex justify-center   items-center gap-8 rounded-[25px] md:hidden ">
             <img src="/assets/icons/tool.png" alt="translate" className=" w-[42px] h-[42px]" />
             <span>AI Assistance</span>
            </div>
