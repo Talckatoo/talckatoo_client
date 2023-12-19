7eb6d1731ba1a28fc4b75459a63e90a945949cc0
tarikbouari <tarikbouari@gmail.com>
2023-12-19 10:45:18 +0000

    Update Features section

--- src/components/LandingPage/Features.tsx
+++ src/components/LandingPage/Features.tsx
@@ -9,7 +9,7 @@ const Features = () => {
           <h2 className=" head-text text-black  md:text-start max-w-[983px] z-[1]">
           Why Choose Talckatoo?
           </h2>
-          <p className="mt-4 max-w-[768px] z-[1] text-center md:text-start">
+          <p className="mt-4 max-w-[533px] md-max:text-[16px] text-[18px] z-[1] text-center md:text-start">
           Many chat platforms restrict you within language barriers. But with
           us, the world is truly at your fingertips.
           </p>
@@ -17,21 +17,21 @@ const Features = () => {
         </div>
         <div className="mt-[5rem] grid sm:grid-cols-2 md:grid-rows-2 gap-4 sm:justify-center ">
            
-           <div className="bg-light-bg p-4 flex items-center gap-8 rounded-[25px]">
+           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex items-center  rounded-[25px]">
             <img src="/assets/icons/traduction.png" alt="translate" className=" w-[42px] h-[42px]" />
-            <span className="text-center"> Text-to-Text Translation</span>       
+            <span className="text-center text-[20px]"> Text-to-Text Translation</span>       
           </div>
-           <div className="bg-light-bg p-4 flex  items-center  rounded-[25px]">
+           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex  items-center  rounded-[25px]">
             <img src="/assets/icons/voice.png" alt="translate" className=" w-[42px] h-[42px]" />
-            <span className="text-center">Voice-to-Voice Translation</span>
+            <span className="text-center text-[20px]">Voice-to-Voice Translation</span>
            </div>
-           <div className="bg-light-bg p-4 flex items-center gap-8 rounded-[25px]  hidden md:block">
+           <div className="bg-light-bg border border-[#EFF0F6] p-4  items-center gap-8 rounded-[25px] hidden md:flex">
             <img src="/assets/icons/friend.png" alt="translate" className=" w-[42px] h-[42px]" />
-            <span>Friend Matching </span>
+            <span className="text-center text-[20px]">Friend Matching </span>
            </div>
-           <div className="bg-light-bg p-4 flex items-center gap-8 rounded-[25px] hidden md:block">
+           <div className="bg-light-bg border border-[#EFF0F6] p-4 items-center gap-8 rounded-[25px] hidden md:flex">
             <img src="/assets/icons/tool.png" alt="translate" className=" w-[42px] h-[42px]" />
-            <span>AI Assistance</span>
+            <span className="text-center text-[20px]">AI Assistance</span>
            </div>
         </div>
   
@@ -40,11 +40,11 @@ const Features = () => {
           <img src="/assets/img/iPhone.png" alt="iPhone" className="md:max-w-[50%] h-full object-contain" />
         </div>
         <div className="grid sm:grid-cols-2 gap-4 sm:justify-center ">
-          <div className="bg-light-bg p-4 flex items-center gap-8 rounded-[25px]  md:hidden ">
+          <div className="bg-light-bg border border-[#EFF0F6] p-4 flex items-center gap-8 rounded-[25px]  md:hidden ">
             <img src="/assets/icons/friend.png" alt="translate" className=" w-[42px] h-[42px]" />
             <span>Friend Matching </span>
            </div>
-           <div className="bg-light-bg p-4 flex items-center gap-8 rounded-[25px] md:hidden ">
+           <div className="bg-light-bg border border-[#EFF0F6] p-4 flex items-center gap-8 rounded-[25px] md:hidden ">
             <img src="/assets/icons/tool.png" alt="translate" className=" w-[42px] h-[42px]" />
             <span>AI Assistance</span>
            </div>
