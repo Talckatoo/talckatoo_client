c34a7a24efa3b8b02cb30c8f62aa4f7215de1d7c
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2024-01-10 22:22:25 +0100

    am pushing don't be mad

--- src/components/shared/SideBar.tsx
+++ src/components/shared/SideBar.tsx
@@ -1,56 +1,48 @@
 import React from "react";
+import { IoSearch } from "react-icons/io5";
 
 const SideBar = () => {
   return (
     <div className="w-2/6 h-full flex">
-      <div className="w-1/5 border-r border-gray-979797 grid grid-cols-1 gap-1 content-between bg-white h-full p-1">
-        <div>
-          <div className="relative">
-            <img
-              src="./src/assests/RectangleBG.png"
-              className="object-contain py-1"
-            />
+      <div className="w-[89px] border-r pt-5 border-gray-979797 grid grid-cols-1 gap-1 content-between bg-white h-full p-1 mb-[2rem]">
+        <div className="flex flex-col  gap-3 w-full">
+          <div className=" bg-secondary-500 mx-2 rounded-[12px]  flex items-center justify-center flex-col">
             <img
               src="./src/assests/chat.png"
-              className="absolute top-1 right-4 z-4 object-contain py-1"
+              className=" top-1 right-4 z-4 object-contain py-2"
             />
           </div>
-          <div className="relative">
-            <img
-              src="./src/assests/RectangleBorder.png"
-              className="object-contain py-1"
-            />
+          <div className=" border-[1px] border-secondary-500 mx-2 rounded-[12px]  flex items-center justify-center flex-col">
             <img
               src="./src/assests/User_alt_fill.png"
-              className="absolute top-1 right-5 z-4 object-contain py-1"
+              className=" top-1 right-5 z-4 object-contain py-2"
             />
           </div>
         </div>
-        <div>
-          <div className="relative">
-            <img
-              src="./src/assests/RectangleBG.png"
-              className="object-contain py-1"
-            />
+        <div className="flex flex-col  gap-3 w-full items-center">
+          <div className=" bg-secondary-500 mx-2 rounded-[12px]  flex items-center justify-center flex-col w-full max-w-[59px]">
             <img
               src="./src/assests/Setting_line.png"
-              className="absolute top-2 right-5 z-4 object-contain py-1"
+              className=" top-2 right-5 z-4 object-contain py-2"
             />
           </div>
-          <img src="./src/assests/userPic.png" className="max-w-52" />
+          <img src="./src/assests/userPic.png" className="max-w-[58px]" />
         </div>
       </div>
 
       {/*<div className="w-4/5 divide-x">two</div>*/}
 
       {/*Second column */}
-      <div className="w-4/5 border-r border-gray-979797">
-        <div className="my-4 ml-4 font-inter font-extrabold text-20">Chats</div>
-        <div className="flex items-center border border-white rounded-md px-1 py-1 bg-secondary-500">
-          <img src="./src/assests/Search.png"/>
-          <input type="text" className="bg-secondary-500 text-white placeholder-white::placeholder focus:outline-none border-0" placeholder="Search"/>
+      <div className="w-4/5 border-r border-gray-979797 px-8 max-lg:px-4 py-4">
+        <div className="my-4 ml-4  font-extrabold text-[20px]">Chats</div>
+        <div className="relative flex">
+          <input
+            type="text"
+            className="bg-secondary-500 pl-12 text-white rounded-lg focus:outline-none placeholder-white::placeholder "
+            placeholder="Search"
+          />
+          <IoSearch className="absolute left-4 top-3 text-gray-200" size={24} />
         </div>
-
       </div>
     </div>
   );
