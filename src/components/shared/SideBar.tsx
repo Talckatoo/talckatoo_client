610e7e5c4cfd01058d80381853f3c11a1c88430c
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2024-01-22 03:55:56 +0100

    pusH

--- src/components/shared/SideBar.tsx
+++ src/components/shared/SideBar.tsx
@@ -45,11 +45,6 @@ const SideBar = ({ socket, refetch }: { socket: any; refetch: any }) => {
       socket.current.on("getFriendRequest", () => {
         refetchFriendsRequest();
       });
-    }
-  }, [socket.current]);
-
-  useEffect(() => {
-    if (socket.current) {
       socket.current.on("getAcceptFriendRequest", () => {
         refetch();
       });
