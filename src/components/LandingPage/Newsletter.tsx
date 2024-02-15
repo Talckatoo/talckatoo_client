df0dc3d46ecffbccc892d212a1ed7446c328494c
said-laasri <saidlaasri3@gmail.com>
2024-02-15 19:32:19 +0100

    Add newsletter subscription

--- src/components/LandingPage/Newsletter.tsx
+++ src/components/LandingPage/Newsletter.tsx
@@ -1,6 +1,35 @@
+import { useState } from 'react';
 import Button from "../../UI/Button";
 
 const Newsletter = () => {
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubscribe = async () => {
+    console.log('Subscribing with email:', email);
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/account/news-letter`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to subscribe');
+      }
+
+      // Optionally, you can handle success scenarios here
+      console.log('Subscribed successfully');
+    } catch (error: any) {
+      console.error('Error subscribing:', error.message);
+    }
+  };
+
   return (
     <>
       <section className="md:mx-10 md:mt-[5rem] bg-[#fafafa] p-16 max-sm:p-6 z-[1] font-inter relative border rounded-xl cursor-pointer">
@@ -12,8 +41,12 @@ const Newsletter = () => {
             type="email"
             placeholder="Enter your email"
             className="w-full md:w-96 border border-gray-700 rounded-l-lg"
+            value={email}
+            onChange={handleEmailChange}
+            name="email" // Add name attribute here
           />
-          <Button className="md:w-44 bg-slate-700 text-yellow-50 rounded-r-lg hover:bg-slate-600 " type={undefined}>Subscribe</Button>
+
+          <Button className="md:w-44 bg-slate-700 text-yellow-50 rounded-r-lg hover:bg-slate-600" onClick={handleSubscribe} type="submit" >Subscribe</Button>
         </div>
       </section>
       <div className="flex justify-center items-center">
