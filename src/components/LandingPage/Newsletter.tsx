fd6d6b77995384dfa124b2a52fb5db6bce0f1497
tarikbouari <tarikbouari@gmail.com>
2024-02-24 14:41:30 +0000

    Set darkmode

--- src/components/LandingPage/Newsletter.tsx
+++ src/components/LandingPage/Newsletter.tsx
@@ -1,41 +1,57 @@
-import { useState } from 'react';
+import { useContext, useState } from "react";
+import { UserContext } from "../../context/user-context";
+
 import Button from "../../UI/Button";
 
 const Newsletter = () => {
-  const [email, setEmail] = useState('');
-
+  const [email, setEmail] = useState("");
+  const { isDarkMode } = useContext(UserContext);
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
   const handleSubscribe = async () => {
-    console.log('Subscribing with email:', email);
+    console.log("Subscribing with email:", email);
     try {
-      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/account/news-letter`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/account/news-letter`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      );
 
       if (!response.ok) {
-        throw new Error('Failed to subscribe');
+        throw new Error("Failed to subscribe");
       }
 
       // Optionally, you can handle success scenarios here
-      console.log('Subscribed successfully');
+      console.log("Subscribed successfully");
     } catch (error: any) {
-      console.error('Error subscribing:', error.message);
+      console.error("Error subscribing:", error.message);
     }
   };
 
   return (
     <>
-      <section className="md:mx-10 md:mt-[5rem]  p-16 max-sm:p-6 z-[1] font-inter relative border rounded-xl cursor-pointer">
+      <section
+        className={`md:mx-10 md:mt-[5rem]  p-16 max-sm:p-6 z-[1] font-inter relative border rounded-xl cursor-pointer`}
+      >
         <h2 className="text-[28px] font-bold text-center mb-5">TALCKATOO</h2>
-        <h2 className="text-[20px] font-bold text-center">Subscribe to our newsletter</h2>
-        <p className="text-center text-[15px] text-gray-500 mb-4">Stay up to date with the roadmap progress, announcements and exclusive discounts feel free to sign up with your email.</p>
+        <h2 className="text-[20px] font-bold text-center">
+          Subscribe to our newsletter
+        </h2>
+        <p
+          className={`text-center text-[15px]  mb-4 ${
+            isDarkMode ? "text-[#E6F7E6]" : "text-gray-500"
+          }`}
+        >
+          Stay up to date with the roadmap progress, announcements and exclusive
+          discounts feel free to sign up with your email.
+        </p>
         <div className="flex flex-row justify-center items-start">
           <input
             type="email"
@@ -46,11 +62,24 @@ const Newsletter = () => {
             name="email" // Add name attribute here
           />
 
-          <button className="md:w-44 h-[46px] bg-black text-white rounded-r-lg hover:text-orange-500" onClick={handleSubscribe} type="submit" >Subscribe</button>
+          <button
+            className="md:w-44 h-[46px] bg-black text-white rounded-r-lg hover:text-orange-500"
+            onClick={handleSubscribe}
+            type="submit"
+          >
+            Subscribe
+          </button>
         </div>
       </section>
       <div className="flex justify-center items-center">
-        We care about the protection of your data. Read our <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://talckatoo.me/terms"> Privacy Policy</a>
+        We care about the protection of your data. Read our{" "}
+        <a
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          href="https://talckatoo.me/terms"
+        >
+          {" "}
+          Privacy Policy
+        </a>
       </div>
     </>
   );
