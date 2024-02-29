ff3d302465432f60f395ca7e69a1994a649321a9
tarikbouari <tarikbouari@gmail.com>
2024-02-29 09:55:56 +0000

    Update Features component

--- src/components/LandingPage/Features.tsx
+++ src/components/LandingPage/Features.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import { useContext } from "react";
 import { MdGTranslate } from "react-icons/md";
+import { UserContext } from "../../context/user-context";
 
 const Features = () => {
+  const { isDarkMode } = useContext(UserContext);
   return (
     <section className=" container md:mt-[8rem] mt-[5rem] ">
       <div className="flex flex-col md:flex-row gap-5 w-full">
         <div className="w-full md:w-[50%]">
           <div className="flex flex-col justify-center items-center md:items-start">
-            <h2 className=" head-text text-black text-center  md:text-start max-w-[983px] z-[1]">
+            <h2
+              className={`head-text text-center  md:text-start max-w-[983px] z-[1]`}
+            >
               Why Choose Talckatoo?
             </h2>
             <p className="mt-4 max-w-[533px] md-max:text-[16px] text-[18px] z-[1] text-center md:text-start">
@@ -25,12 +29,16 @@ const Features = () => {
                 />{" "}
               </div>
               <div className="w-3/4 flex justify-center">
-                <span className="text-center max-sm:text-[18px] text-[20px]">
+                <span
+                  className={`text-center max-sm:text-[18px] text-[20px] ${
+                    isDarkMode ? "text-black" : "text-black"
+                  }`}
+                >
                   Text-to-Text Translation
                 </span>
               </div>
             </div>
-            <div className="bg-light-bg shadow-lg border gap-1 max-sm:gap-4 border-[#EFF0F6] p-4 flex justify-around items-center rounded-[25px] w-100 md:flex-row flex-col">
+            <div className="bg-light-bg  text-black shadow-lg border gap-1 max-sm:gap-4 border-[#EFF0F6] p-4 flex justify-around items-center rounded-[25px] w-100 md:flex-row flex-col">
               <div className="w-1/4 flex justify-center items-center">
                 <img
                   src="/assets/icons/voice.png"
@@ -44,7 +52,7 @@ const Features = () => {
                 </span>
               </div>
             </div>
-            <div className="md:bg-light-bg md:shadow-lg md:border md:gap-1 md:max-sm:gap-4 md:border-[#EFF0F6] md:p-4 md:flex md:justify-around md:items-center md:rounded-[25px] md:flex-row hidden flex-col">
+            <div className="md:bg-light-bg md:shadow-lg text-black md:border md:gap-1 md:max-sm:gap-4 md:border-[#EFF0F6] md:p-4 md:flex md:justify-around md:items-center md:rounded-[25px] md:flex-row hidden flex-col">
               <div className="w-1/4 flex justify-center items-center">
                 <img
                   src="/assets/icons/friend.png"
@@ -58,7 +66,7 @@ const Features = () => {
                 </span>
               </div>
             </div>
-            <div className="md:bg-light-bg md:shadow-lg md:border md:gap-1 md:max-sm:gap-4 md:border-[#EFF0F6] md:p-4 md:flex md:justify-around md:items-center md:rounded-[25px] md:flex-row hidden flex-col">
+            <div className="md:bg-light-bg md:shadow-lg md:border text-black md:gap-1 md:max-sm:gap-4 md:border-[#EFF0F6] md:p-4 md:flex md:justify-around md:items-center md:rounded-[25px] md:flex-row hidden flex-col">
               <div className="w-1/4 flex justify-center items-center">
                 <img
                   src="/assets/icons/tool.png"
