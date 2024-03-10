cd91d4b30530b9cb0a1dab249c8ec1848c7810d0
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2024-03-10 19:42:57 +0000

    >>>

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -11,6 +11,8 @@ import languagesArray from "../util/languages";
 import { setAuth } from "../redux/features/user/authSlice";
 import { MdOutlineSecurity } from "react-icons/md";
 import { UserContext } from "../context/user-context";
+import { setMessages } from "../redux/features/messages/messageSlice";
+import { setConversation } from "../redux/features/conversation/conversationSlice";
 
 interface FormData {
   name: string;
@@ -29,7 +31,7 @@ interface FormErrors {
 
 export const SignUp = () => {
   const navigate = useNavigate();
-  const { userEmail } = useContext(UserContext); 
+  const { userEmail } = useContext(UserContext);
   const [formData, setFormData] = React.useState<FormData>({
     name: "",
     password: "",
@@ -43,7 +45,6 @@ export const SignUp = () => {
   const [formErrors, setFormErrors] = React.useState<FormErrors>({});
   const [error, setError] = useState<string | null>(null);
   const refs = useRef<Array<HTMLInputElement>>([]);
-
 
   const validateForm = (): boolean => {
     let isValid = true;
@@ -90,6 +91,9 @@ export const SignUp = () => {
         const user = response?.data?.user;
         localStorage.setItem("token", token as string);
         localStorage.removeItem("persist:root");
+        dispatch(setMessages([]));
+        dispatch(setConversation({ conversationId: null, selectedId: null }));
+
         dispatch(setAuth(user));
         toast.success("User signed up");
         navigate("/chat");
@@ -106,7 +110,6 @@ export const SignUp = () => {
     }
   };
 
-
   return (
     <section className="relative bg-white h-full w-full font-inter">
       <div className="bg-white fixed top-0 left-0 w-full h-full -z-20"></div>
@@ -117,112 +120,121 @@ export const SignUp = () => {
       />
       <NavBar showSign={false} />
       {/* End of Nav bar section */}
-  
-        <h1 className="head-text text-center mt-[6rem] mb-6 text-black">
-          Join Talckatoo Today!
-        </h1>
-        {/* Sign up form  */}
-        <form
-          className="flex flex-col items-center justify-center max-w-[400px] mx-auto"
-          onSubmit={handleSubmit}
+
+      <h1 className="head-text text-center mt-[6rem] mb-6 text-black">
+        Join Talckatoo Today!
+      </h1>
+      {/* Sign up form  */}
+      <form
+        className="flex flex-col items-center justify-center max-w-[400px] mx-auto"
+        onSubmit={handleSubmit}
+      >
+        <Input
+          // label="Username"
+          type="text"
+          name="name"
+          placeholder="Username"
+          value={formData.name}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setFormData({ ...formData, name: e.target.value })
+          }
+          className="bg-transparent border-[#33363A] z-[1] rounded-lg text-black"
+          error={formErrors.name}
+          label={""}
+          id={""}
+        />
+
+        <Input
+          // label="Password"
+          name="password"
+          type="password"
+          placeholder="Password (at least 8 characters)"
+          value={formData.password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+          className="bg-transparent border-[#33363A] z-[1] rounded-lg text-black"
+          error={formErrors.password}
+          label={""}
+          id={""}
+        />
+        <Input
+          // label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+          value={formData.confirmPassword}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setFormData({ ...formData, confirmPassword: e.target.value })
+          }
+          className="bg-transparent border-[#33363A] z-[1] rounded-lg"
+          error={formErrors.confirmPassword}
+          label={""}
+          id={""}
+        />
+
+        <select
+          className={`rounded-lg p-3 my-4 w-full border text-black relative text-[16px] focus:outline-none z-[1] ${
+            formErrors.selectedLanguage ? "border-red-500" : ""
+          } bg-transparent border-[#33363A]`}
+          value={selectedLanguage}
+          onChange={(e) => setSelectedLanguage(e.target.value)}
         >
-          <Input
-            // label="Username"
-            type="text"
-            name="name"
-            placeholder="Username"
-            value={formData.name}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setFormData({ ...formData, name: e.target.value })
-            }
-            className="bg-transparent border-[#33363A] z-[1] rounded-lg text-black"
-            error={formErrors.name}
-            label={""}
-            id={""}
-          />
-
-          <Input
-            // label="Password"
-            name="password"
-            type="password"
-            placeholder="Password (at least 8 characters)"
-            value={formData.password}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setFormData({ ...formData, password: e.target.value })
-            }
-            className="bg-transparent border-[#33363A] z-[1] rounded-lg text-black"
-            error={formErrors.password}
-            label={""}
-            id={""}
-          />
-          <Input
-            // label="Confirm Password"
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirm Password"
-            value={formData.confirmPassword}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setFormData({ ...formData, confirmPassword: e.target.value })
-            }
-            className="bg-transparent border-[#33363A] z-[1] rounded-lg"
-            error={formErrors.confirmPassword}
-            label={""}
-            id={""}
-          />
-
-          <select
-            className={`rounded-lg p-3 my-4 w-full border text-black relative text-[16px] focus:outline-none z-[1] ${
-              formErrors.selectedLanguage ? "border-red-500" : ""
-            } bg-transparent border-[#33363A]`}
-            value={selectedLanguage}
-            onChange={(e) => setSelectedLanguage(e.target.value)}
-          >
-            <option value="" disabled hidden>
-              Select Your Language
+          <option value="" disabled hidden>
+            Select Your Language
+          </option>
+          {languagesArray?.map(({ code, language }) => (
+            <option key={code} value={code} className="bg-white">
+              {language}
             </option>
-            {languagesArray?.map(({ code, language }) => (
-              <option key={code} value={code} className="bg-white">
-                {language}
-              </option>
-            ))}
-          </select>
-          {formErrors.selectedLanguage && (
-            <p className="mt-2 text-[16px] md:text-[18px] text-red-500">
-              {formErrors.selectedLanguage}
-            </p>
-          )}
-
-          <Button
-            type="submit"
-            className="bg-black text-white w-full h-[48px] mt-[2rem] z-[1] rounded-lg"
-            onClick={() => {}}
-          >
-            Sign Up
-          </Button>
-
-          <p className="text-black mt-4 z-[1]">
-            Already have an account?{" "}
-            <Link
-              className="text-black cursor-pointer rounded-lg underline font-semibold"
-              to="/sign-in"
-            >
-              Sign In
-            </Link>
+          ))}
+        </select>
+        {formErrors.selectedLanguage && (
+          <p className="mt-2 text-[16px] md:text-[18px] text-red-500">
+            {formErrors.selectedLanguage}
           </p>
-        </form>
-        <div className="flex justify-center items-center mt-6 py-4 text-[#696868] gap-1">
-          <div className="flex gap-1 items-center">
-            <MdOutlineSecurity />
-            <span>
-            By signing up, you agree to our 
-            </span>
-           </div>
-           <div className="flex gap-2">
-           <p className="text-[blue] cursor-pointer" onClick={()=> navigate("/terms")}>Terms of Service & </p>
-           <p className="text-[blue] cursor-pointer" onClick={()=> navigate("/privacy")}> Privacy</p>
-           </div>
+        )}
+
+        <Button
+          type="submit"
+          className="bg-black text-white w-full h-[48px] mt-[2rem] z-[1] rounded-lg"
+          onClick={() => {}}
+        >
+          Sign Up
+        </Button>
+
+        <p className="text-black mt-4 z-[1]">
+          Already have an account?{" "}
+          <Link
+            className="text-black cursor-pointer rounded-lg underline font-semibold"
+            to="/sign-in"
+          >
+            Sign In
+          </Link>
+        </p>
+      </form>
+      <div className="flex justify-center items-center mt-6 py-4 text-[#696868] gap-1">
+        <div className="flex gap-1 items-center">
+          <MdOutlineSecurity />
+          <span>By signing up, you agree to our</span>
         </div>
-    
+        <div className="flex gap-2">
+          <p
+            className="text-[blue] cursor-pointer"
+            onClick={() => navigate("/terms")}
+          >
+            Terms of Service &{" "}
+          </p>
+          <p
+            className="text-[blue] cursor-pointer"
+            onClick={() => navigate("/privacy")}
+          >
+            {" "}
+            Privacy
+          </p>
+        </div>
+      </div>
+
       <br />
       <br />
       <br />
