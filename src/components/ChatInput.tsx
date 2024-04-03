0a8e6713647fd7cf51b36f9c7fcc85d1153a1e5a
tarikbouari <tarikbouari@gmail.com>
2024-04-03 18:57:39 +0000

    Update chatInput

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -182,8 +182,8 @@ const ChatInput = ({
             onKeyDown={handleSendMessageKeyDown as any}
             id=""
             placeholder="Type your message or type @birdie to call AI Assistant"
-            className={`mb-0 rounded-t-[20px]   border border-[#0E131D] 
-            ${isDarkMode ? "bg-[#25282C]" : "bg-white"}
+            className={`mb-0 rounded-t-[20px]   border  
+            ${isDarkMode ? "bg-[#282828] border-[#141414]" : "bg-white border-[#0E131D]"}
             ${
               messageText.startsWith(AIcall)
                 ? "text-gray-700 italic font-semibold"
@@ -192,8 +192,8 @@ const ChatInput = ({
           />
 
           <div
-            className={`flex justify-between items-center relative bottom-[2rem] py-3 rounded-b-[20px] px-2 ${
-              isDarkMode ? "bg-[#181818]" : "bg-[#25282C] "
+            className={`flex justify-between items-center relative bottom-[2rem] py-3 border rounded-b-[20px] px-2 ${
+              isDarkMode ? "bg-[#181818] border-[#141414]" : "bg-[#25282C] "
             }`}
           >
             <form onSubmit={handleSendMessage} className="absolute right-4 ">
@@ -201,7 +201,7 @@ const ChatInput = ({
                 <IoSend className="text-white text-[20px]" />
               </button>
             </form>
-            <div className="w-[200px] flex items-center">
+            <div className="w-[200px] flex items-center ">
               <div className="flex">
                 <VoiceMessage
                   socket={socket}
