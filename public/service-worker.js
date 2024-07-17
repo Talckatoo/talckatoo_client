1904bcef64a1160c627a10ff39a47f6fa4e7cfd7
said-laasri <saidlaasri3@gmail.com>
2024-07-17 13:53:15 +0100

    Fix some eslintrc error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 // public/service-worker.js
-self.addEventListener("push", function (event) {
+self.addEventListener('push', (event) => {
   const options = {
     body: event.data.text(),
-    icon: "/icon.png",
-    badge: "/badge.png",
+    icon: '/icon.png',
+    badge: '/badge.png',
   };
   event.waitUntil(
-    self.registration.showNotification("Notification Title", options)
+    self.registration.showNotification('Notification Title', options),
   );
 });
